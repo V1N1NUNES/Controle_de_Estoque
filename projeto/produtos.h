cef9acd3cf3d5b5bc2b1e5e78432a0f324f37527
vinicius <viniamenic@gmail.com>
2024-08-15 00:07:16 -0300

    continuar fazendo a função 'atualizar' e começar a fazer 'remover_produto'

--- projeto/produtos.h
+++ projeto/produtos.h
@@ -1,6 +1,6 @@
 // Desenvolver um sistema básico de para cadastro de produtos em estoque
 
-// bibliotecas
+// BIBLIOTECAS
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,7 +8,7 @@
 #include <conio.h>
 #define tam 200
 
-// declarações
+// DECLARAÇÕES
 typedef struct
 {
     char nome[tam];
@@ -25,7 +25,8 @@ int quant_tec = 0;
 int quant_cosm = 0;
 int quant_ali = 0;
 
-// projeto de funções
+
+// PROJETO DE FUNÇÕES
 void menu();
 void cadastrar_produto();
 void listar_produto();
@@ -33,21 +34,21 @@ void atualizar();
 void remover_produto();
 void limpar_buffer();
 void remover_newline(char *str);
-
 // funções p/ arquivos
 // void escrever()
 // void ler()
 // void mostrar()
 
-// funções
-void limpar_buffer()
+
+// FUNÇÕES
+void limpar_buffer()//(ok)
 { //(ok)
     int clean;
     while ((clean = getchar()) != '\n' && clean != EOF)
         ;
 }
 
-void remover_newline(char *str)
+void remover_newline(char *str)//(ok)
 { //(ok)
     size_t len = strlen(str);
     if (len > 0 && str[len - 1] == '\n')
@@ -56,7 +57,7 @@ void remover_newline(char *str)
     }
 }
 
-void cadastrar_produto()
+void cadastrar_produto()//(process...)
 {
     char categoria[20];
     char resposta[10];
@@ -120,7 +121,7 @@ void cadastrar_produto()
     }
 }
 
-void listar_produto()
+void listar_produto()//(process...)
 {
     char resposta[10];
     char categoria[20];
@@ -134,14 +135,14 @@ void listar_produto()
 
         if (strcmp(categoria, "Tecnologia") == 0 || strcmp(categoria, "tecnologia") == 0)
         {
-            if(quant_tec <= 0)
+            if (quant_tec <= 0)
             {
                 char resp[10];
                 printf("\nestoque de tecnologia vazio.\nDeseja listar outra categoria?\n");
                 fgets(resp, sizeof(resp), stdin);
                 remover_newline(resp);
 
-                if(strcmp(resp, "sim") == 0 || strcmp(resp, "Sim") == 0)
+                if (strcmp(resp, "sim") == 0 || strcmp(resp, "Sim") == 0)
                 {
                     listar_produto();
                     return;
@@ -162,7 +163,7 @@ void listar_produto()
 
             printf("Pressione qualquer tecla para continuar...\n");
             getch();
-            //continue;
+            // continue;
         }
         else
         {
@@ -193,12 +194,108 @@ void listar_produto()
     menu();
 }
 
-// void atualizar(){
+void atualizar()//continua...
+{
+    char categoria[20];
+    char resposta[10];
 
-// void remover_produto(){
+    do
+    {
+        printf("\nDigite a categoria do produto que deseja editar:\n");
+        limpar_buffer();
+        fgets(categoria, sizeof(categoria), stdin);
+        remover_newline(categoria);
 
-void menu() // função listar produto desabilitada
-{           //(process)
+        if (strcmp(categoria, "tecnologia") == 0 || strcmp(categoria, "Tecnologia") == 0)
+        {
+            char nome[tam];
+
+            if (quant_tec <= 0)
+            {
+                char resp[10];
+                printf("\nEstoque de tecnologia vazio.\nDeseja editar outro produto?\n");
+                fgets(resp, sizeof(resp), stdin);
+                remover_newline(resp);
+
+                if (strcmp(resp, "sim") == 0 || strcmp(resp, "Sim") == 0)
+                {
+                    atualizar();
+                    return;
+                }
+                else
+                {
+                    printf("\nVoltando ao menu...\n\n");
+                    sleep(1.2);
+                    menu();
+                    return;
+                }
+            }
+            printf("\nDigite o nome do produto que deseja modificar:\n");
+            fgets(nome, sizeof(nome), stdin);
+            remover_newline(nome);
+
+            // laço para procurar o produto
+            for (int i = 0; i < quant_tec; i++)
+            {
+                if (strcmp(nome, estoque.tecnologia[i].nome) == 0)
+                {
+                    char mudanca[30];
+                    char novo[tam];
+                    char resp[10];
+
+                    do
+                    {
+                        printf("\nOque deseja mudar?\n- Nome\n- Preço\n");
+                        fgets(mudanca, sizeof(mudanca), stdin);
+                        remover_newline(mudanca);
+
+                        if (strcmp(mudanca, "nome") == 0 || strcmp(mudanca, "Nome") == 0)
+                        {
+
+                            printf("\nDigite o novo nome do produto:\n");
+                            fgets(novo, sizeof(novo), stdin);
+                            remover_newline(novo);
+
+                            strcpy(estoque.tecnologia[i].nome, novo);
+
+                            printf("\nNome alterado com sucesso!!\n\n");
+                            printf("\nDeseja alterar outro produto no estoque de tecnologia?\n");
+                            fgets(resp, sizeof(resp), stdin);
+                            remover_newline(resp);
+
+                            if (strcmp(resp, "sim") == 0 || strcmp(resp, "Sim") == 0)
+                            {
+                                atualizar();
+                                return;
+                            }
+                            else
+                            {
+                                printf("Voltando para o menu...");
+                                sleep(1.2);
+                                menu();
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            printf("\nNOme digitado não encontrado..\nDeseja modificar outro produto?\n");
+                        }
+                    } while (strcmp(resp, "sim") == 0 || strcmp(resp, "Sim") == 0);
+
+                    menu();
+                }
+            }
+        }
+    } while(strcmp(resposta, "sim") == 0 || strcmp(resposta, "Sim") == 0);
+    
+    menu();
+    return;
+}
+
+// void remover_produto()//(incompleted)
+
+void menu()//(process)
+{           
     int opcao;
     do
     {
